1c7c2ce7036aaa13a4b2999ec19b8c1375e209f2
anprivitera <andrea.privitera@mailbox.org>
2023-09-03 12:09:14 -0700

    testing variable declaration

--- scripts/testinges5.js
+++ scripts/testinges5.js
@@ -1,7 +1,8 @@
 "use strict";
+let newNumbers;
 const numbers = [1, 3, 5];
 function multiplier(numbers) {
     return numbers * 2;
 }
-const newNumbers = numbers.map(multiplier)
+newNumbers = numbers.map(multiplier)
 document.write(`The old numbers were ${numbers}. The new numbers are ${newNumbers}`)
