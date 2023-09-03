dc481f6d32b3a528e426c6f5d1395d73f0bf01f5
anprivitera <andrea.privitera@mailbox.org>
2023-09-03 12:18:44 -0700

    declaring multiple variables at once

--- scripts/testinges5.js
+++ scripts/testinges5.js
@@ -1,8 +1,9 @@
 "use strict";
 let newNumbers;
+let message1 = "old numbers", message2 = "new numbers" // this is not recommended
 const numbers = [1, 3, 5];
 function multiplier(numbers) {
     return numbers * 2;
 }
 newNumbers = numbers.map(multiplier)
-document.write(`The old numbers were ${numbers}. The new numbers are ${newNumbers}`)
+document.write(`The ${message1} were ${numbers}. The ${message2} are ${newNumbers}`)
